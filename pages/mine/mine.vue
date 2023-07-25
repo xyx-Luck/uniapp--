3b7259321ff5e1e29d9153a291fcb54dccada9a7
谢亚希 <yaxi9204@126.com>
2023-07-25 17:08:54 +0800

    '架构搭建'

---
+++ pages/mine/mine.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<view>我的</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/notes/notes.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<view>小纸条</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
