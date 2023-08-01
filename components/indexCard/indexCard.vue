3a49b19482c1d3a376b427fbae74b3f74aba48fc
谢亚希 <yaxi9204@126.com>
2023-08-01 15:22:50 +0800

    ''

--- components/indexCard/indexCard.vue
+++ components/indexCard/indexCard.vue
@@ -1,68 +1,73 @@
 <template>
 	<view>
-		<view class="indexContent" v-for="(item,index) in indexCardList">
-			<view class="indexAvater">
-				<view>
-					<image :src="item.avater" mode="widthFix"></image>
-					<text>{{item.userName}}</text>
-				</view>
-				<view>
-					<view class="guanzhu">
-						<view :class="item.isFollow==false?'icon iconfont icon-zengjia':'active'">
-							{{item.isFollow==false?'关注':'已关注'}}</view>
-					</view>
-					<view class="icon iconfont icon-guanbi" color="#D5D5D5"></view>
 		
+				<view class="indexContent">
+					<view class="indexAvater">
+						<view>
+							<image :src="item.avater" mode="widthFix"></image>
+							<text>{{item.userName}}</text>
+						</view>
+						<view>
+							<view class="guanzhu">
+								<view :class="item.isFollow==false?'icon iconfont icon-zengjia':'active'">
+									{{item.isFollow==false?'关注':'已关注'}}
+								</view>
+							</view>
+							<view class="icon iconfont icon-guanbi" color="#D5D5D5"></view>
+
+						</view>
+					</view>
+					<view class="indexTitle">
+						{{item.title}}
+					</view>
+					<view class="indexImg">
+						<image :src="item.titlePic"></image>
+						<template v-if="item.type!='img'">
+							<view class="indexPlayer icon iconfont icon-bofang"></view>
+							<view class="indexPlayTime">{{item.playTime}}次播放 {{item.videoTime}}</view>
+						</template>
+					</view>
+					<view class="indexCommit">
+						<view>
+							<view>
+								<view
+									:class="item.small.index==1?'icon iconfont icon-icon_xiaolian-mian active':'icon iconfont icon-icon_xiaolian-mian' ">
+								</view>
+								<text>{{item.small.agree}}</text>
+							</view>
+							<view>
+								<view class="icon iconfont icon-kulian"></view>
+								<text>{{item.small.disagree}}</text>
+							</view>
+						</view>
+						<view>
+							<view>
+								<view class="icon iconfont icon-pinglun1"></view>
+								<text>{{item.comment}}</text>
+							</view>
+							<view>
+								<view class="icon iconfont icon-zhuanfa"></view>
+								<text>{{item.share}}</text>
+							</view>
+						</view>
+					</view>
 				</view>
-			</view>
-			<view class="indexTitle">
-				{{item.title}}
-			</view>
-			<view class="indexImg">
-				<image :src="item.titlePic"></image>
-				<template v-if="item.type!='img'">
-					<view class="indexPlayer icon iconfont icon-bofang"></view>
-					<view class="indexPlayTime">{{item.playTime}}次播放 {{item.videoTime}}</view>
-				</template>
-			</view>
-			<view class="indexCommit">
-				<view>
-					<view>
-						<view :class="item.small.index==1?'icon iconfont icon-icon_xiaolian-mian active':'icon iconfont icon-icon_xiaolian-mian' "></view>
-						<text>{{item.small.agree}}</text>
-					</view>
-					<view>
-						<view class="icon iconfont icon-kulian"></view>
-						<text>{{item.small.disagree}}</text>
-					</view>
-				</view>
-				<view>
-					<view>
-						<view class="icon iconfont icon-pinglun1"></view>
-						<text>{{item.comment}}</text>
-					</view>
-					<view>
-						<view class="icon iconfont icon-zhuanfa"></view>
-						<text>{{item.share}}</text>
-					</view>
-				</view>
-			</view>
-		</view>
+			
 	</view>
 </template>
 
 <script>
 	export default {
-		name:"indexCard",
-		props:{
-		  	indexCardList:{
-				type:Array,
-				default:[]
+		name: "indexCard",
+		props: {
+			item: {
+				type: Object,
+				default:{}
 			}
 		},
 		data() {
 			return {
-				
+
 			};
 		}
 	}
@@ -110,11 +115,13 @@
 		background: #F4F4F4;
 		padding: 6upx;
 	}
-    .guanzhu>uni-view.active{
+
+	.guanzhu>uni-view.active {
 		background: #00ff00;
 		padding: 6upx;
-		color:#ffffff
+		color: #ffffff
 	}
+
 	.guanzhu+uni-view {
 		margin-left: 16upx;
 	}
@@ -168,14 +175,19 @@
 	.indexCommit>uni-view>uni-view:nth-child(2) {
 		margin-left: 8px;
 	}
-    .indexCommit>uni-view>uni-view>uni-view{
-		color:#D5D5D5;
+
+	.indexCommit>uni-view>uni-view>uni-view {
+		color: #D5D5D5;
 	}
-	dexCommit>uni-view>uni-view>uni-view.active{
-		color:#f8ec63;
+
+	dexCommit>uni-view>uni-view>uni-view.active {
+		color: #f8ec63;
 	}
+
 	.indexCommit>uni-view>uni-view>uni-text {
 		margin-left: 5px;
 		color: #D5D5D5;
 	}
+
+	
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,32 @@
 <template>
 	<view>
-		<!-- aaaa -->
-		<topScrollNav scrollX='true' :tabBars="tabBars" :currentIndex="currentIndex" @changeTabIndex="changeTabIndex"></topScrollNav>
-		<indexCard :indexCardList="indexCardList"></indexCard>
+		<!-- 顶部滚动导航 -->
+		<topScrollNav scrollX='true' :tabBars="tabBars" :currentIndex="currentIndex" @changeTabIndex="changeTabIndex">
+		</topScrollNav>
+		<!-- <view class="uni-tab-bar">
+			<swiper class="swiper-box" 
+				:style="{height:swiperHeight+'px'}" 
+				:current="current"
+				@change="changeCurrent"
+			>
+				<swiper-item v-for="(swiper,index) in indexCardList" :key="index">
+					<scroll-view scroll-y  class="list">
+						<indexCard :indexCardList="swiper.list"></indexCard>
+					</scroll-view>
+				</swiper-item>
+			</swiper>
+		</view> -->
+		<swiper current=0 class="swiper-box" :style="{height:swiperHeight+'px'}"  style="flex: 1;" :duration="300" @change="changeCurrent">
+		    <swiper-item class="swiper-item" v-for="(tab,index) in indexCardList" :key="index">
+				<scroll-view class="scroll-v list scroll-view swiper-scroll" scroll-y="true">
+					<block v-for="(item,index1) in tab.list" :key="index1">
+						<!-- <view>{{item}}</view> -->
+						<indexCard :item="item"></indexCard>
+					</block>
+					
+				</scroll-view>
+		    </swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -10,41 +34,82 @@
 	import indexCard from '@/components/indexCard/indexCard.vue'
 	import topScrollNav from '@/components/topScrollNav/topScrollNav.vue'
 	export default {
-		components: [indexCard,topScrollNav],
+		components: [indexCard, topScrollNav],
 		data() {
 			return {
-				currentIndex:0,
+				currentIndex: 0,
+				tabIndex: 0,
+				swiperHeight: 0,
 				indexCardList: [{
-						avater: "../../static/demo/userpic/2.jpg",
-						userName: "鲁大师",
-						isFollow: false, //是否关注
-						title: "走出去才发现你跟别人差的不是一点半点",
-						type: "img", //img——>图片模式   video——>视频模式
-						titlePic: "../../static/demo/datapic/12.jpg",
-						small: {
-							index: 1, //0代表没有操作，1代表顶,2代表踩；
-							agree: 10,
-							disagree: 10,
-						},
-						comment: 10, //评论
-						share: 10, //分享
-					}, {
-						avater: "../../static/demo/userpic/1.jpg",
-						userName: "鲁大师",
-						isFollow: true, //是否关注
-						title: "如何用手杖改变你的一生",
-						type: "video", //img——>图片模式   video——>视频模式
-						titlePic: "../../static/demo/datapic/11.jpg",
-						playTime: "20万", //播放次数
-						videoTime: "2:47",
-						small: {
-							index: 2, //0:没有操作，1:顶,2:踩；
-							agree: 10,
-							disagree: 10,
-						},
-						comment: 10, //评论
-						share: 10, //分享
+						list: [{
+								avater: "../../static/demo/userpic/2.jpg",
+								userName: "鲁大师",
+								isFollow: false, //是否关注
+								title: "走出去才发现你跟别人差的不是一点半点",
+								type: "img", //img——>图片模式   video——>视频模式
+								titlePic: "../../static/demo/datapic/12.jpg",
+								small: {
+									index: 1, //0代表没有操作，1代表顶,2代表踩；
+									agree: 10,
+									disagree: 10,
+								},
+								comment: 10, //评论
+								share: 10, //分享
+							},
+							{
+								avater: "../../static/demo/userpic/1.jpg",
+								userName: "鲁大师",
+								isFollow: true, //是否关注
+								title: "如何用手杖改变你的一生",
+								type: "video", //img——>图片模式   video——>视频模式
+								titlePic: "../../static/demo/datapic/11.jpg",
+								playTime: "20万", //播放次数
+								videoTime: "2:47",
+								small: {
+									index: 2, //0:没有操作，1:顶,2:踩；
+									agree: 10,
+									disagree: 10,
+								},
+								comment: 10, //评论
+								share: 10, //分享
+							},
+						],
+						list: [{
+								avater: "../../static/demo/userpic/3.jpg",
+								userName: "鲁大师",
+								isFollow: false, //是否关注
+								title: "走出去才发现你跟别人差的不是一点半点",
+								type: "img", //img——>图片模式   video——>视频模式
+								titlePic: "../../static/demo/datapic/10.jpg",
+								small: {
+									index: 1, //0代表没有操作，1代表顶,2代表踩；
+									agree: 10,
+									disagree: 10,
+								},
+								comment: 10, //评论
+								share: 10, //分享
+							},
+							{
+								avater: "../../static/demo/userpic/4.jpg",
+								userName: "鲁大师",
+								isFollow: true, //是否关注
+								title: "如何用手杖改变你的一生",
+								type: "video", //img——>图片模式   video——>视频模式
+								titlePic: "../../static/demo/datapic/11.jpg",
+								playTime: "20万", //播放次数
+								videoTime: "2:47",
+								small: {
+									index: 2, //0:没有操作，1:顶,2:踩；
+									agree: 10,
+									disagree: 10,
+								},
+								comment: 10, //评论
+								share: 10, //分享
+							},
+						],
 					},
+
+
 
 				],
 				tabBars: [{
@@ -78,13 +143,44 @@
 			}
 		},
 		onLoad() {
-
+			//设置内容的高度
+			uni.getSystemInfo({
+				success: (res) => {
+					const height = res.windowHeight - uni.upx2px(100)
+					this.swiperHeight = height
+				}
+			})
 		},
 		methods: {
-             changeTabIndex(index){
-				 console.log(index)
-				 this.currentIndex=index
-			 }
+			changeTabIndex(index) {
+				console.log(index)
+				this.currentIndex = index
+				this.tabIndex = index
+			},
+			changeCurrent(e) {
+				console.log('swiperIndex',e.detail.current)
+				this.tabIndex = e.detail.current
+			}
 		}
 	}
 </script>
+<style>
+	.swiper-box {
+		flex: 1;
+		/* height:700px; */
+	}
+
+	.swiper-item {
+		flex: 1;
+		flex-direction: row;
+	}
+
+	.scroll-v {
+		flex: 1;
+		/* #ifndef MP-ALIPAY */
+		flex-direction: column;
+		/* #endif */
+		width: 750rpx;
+		width: 100%;
+	}
+</style>
